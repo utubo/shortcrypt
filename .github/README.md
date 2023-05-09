f4d3a0a9d0b9160272d8bf66bb61c9ed611311bd
utubo <utbutb@gmail.com>
2023-05-09 13:32:00 +0900

    feat: set default random key

--- .github/README.md
+++ .github/README.md
@@ -2,12 +2,14 @@
 
 A easy page to RSA encrypt short text.
 
+**⚠Caution**
+- This is not secure. (This is easily brute-force attacked.)
+- The URLs contains all encrypted data, so better not to have long text.
+
 ## Usage
 1. Bob (he wants to receive a message) opens [here](https://utubo.github.io/shortcrypt/), inputs a key(passphrase) to generate the URL of the page for encryption, and publishes the URL.
 2. Alice (she wants to send a message) opens the URL published by Bob, inputs the text and passes the generated URL to Bob.
 3. Bob opens the URL received from Alice, and can input the key(passphrase) to decrypt the text.
-
-⚠The URLs contains the encrypted data, so better not to have long text.
 
 ## Thx
 Shortcrypt is powered by [cryptico](https://github.com/wwwtyro/cryptico).
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const BITS = 512;
 const PREFIX_LEN = 4;
+const DEFAULT_KEY_LEN = 32;
 
 let prefix = ''; // for generate a different URL each time with the same password.
 let data = location.search.substr(4);
@@ -8,8 +9,45 @@ if (data && data[PREFIX_LEN] === '=') {
   data = data.substr(PREFIX_LEN + 1);
 }
 
-if (location.search.startsWith('?enc')) {
+const setupTopPage = () => {
+  startPage.classList.remove('hidden');
+
+  const generateEncURL = e => {
+    const prefix = Math.random()
+      .toString(36)
+      .substr(2, PREFIX_LEN)
+      .padEnd(PREFIX_LEN, 'a');
+    const passPhrase = prefix + encPass.value;
+    const privateKey = cryptico.generateRSAKey(passPhrase, BITS);
+    const publicKey = cryptico.publicKeyString(privateKey);
+    const url = new URL(location.href);
+    url.hash = encodeURI(bob.value);
+    url.search = '?enc' + prefix + '=' + publicKey;
+    receiveBoxLink.href = url.href;
+    receiveBoxLink.textContent = url.href;
+  }
+
+  encPass.addEventListener('input', generateEncURL);
+  bob.addEventListener('input', generateEncURL);
+
+  setTimeout(() => {
+    if (encPass.value === '') {
+      while (encPass.value.length < DEFAULT_KEY_LEN) {
+        encPass.value += Math.random().toString(36).substr(2, DEFAULT_KEY_LEN);
+      }
+      generateEncURL();
+    }
+  }, 500);
+
+  showEncPass.addEventListener('change', () => {
+    encPass.setAttribute('type', showEncPass.checked ? 'text': 'password');
+  });
+  showEncPass.checked = false;
+};
+
+const setupEncPage = () => {
   encPage.classList.remove('hidden');
+
   if (location.hash) {
     const name = decodeURI(location.hash.replace(/^#|\?.*$/g, ''));
     for (const b of document.getElementsByClassName('to-bob')) {
@@ -17,70 +55,62 @@ if (location.search.startsWith('?enc')) {
       b.classList.remove('hidden');
     }
   }
-} else if (location.search.startsWith('?dec')) {
-  decPage.classList.remove('hidden');
-  decryptedText.textContent = location.search.substr(1);
-} else {
-  startPage.classList.remove('hidden');
-}
 
-const generateEncURL = e => {
-  const prefix = Math.random()
-    .toString(36)
-    .substr(2, PREFIX_LEN)
-    .padEnd(PREFIX_LEN, 'a');
-  const passPhrase = prefix + encPass.value;
-  const privateKey = cryptico.generateRSAKey(passPhrase, BITS);
-  const publicKey = cryptico.publicKeyString(privateKey);
-  const url = new URL(location.href);
-  url.hash = encodeURI(bob.value);
-  url.search = '?enc' + prefix + '=' + publicKey;
-  receiveBoxLink.href = url.href;
-  receiveBoxLink.textContent = url.href;
-}
-encPass.addEventListener('input', generateEncURL);
-bob.addEventListener('input', generateEncURL);
-
-plainText.addEventListener('input', e => {
-  const publicKey = data;
-  const crypted = cryptico.encrypt(encodeURI(plainText.value), publicKey);
-  if (crypted.status === 'success') {
-    const url = new URL(location.href);
-    url.search = '?dec';
-    if (prefix) {
-      url.search += prefix + '=';
+  plainText.addEventListener('input', e => {
+    const publicKey = data;
+    const crypted = cryptico.encrypt(encodeURI(plainText.value), publicKey);
+    if (crypted.status === 'success') {
+      const url = new URL(location.href);
+      url.search = '?dec';
+      if (prefix) {
+        url.search += prefix + '=';
+      }
+      url.search += crypted.cipher;
+      url.hash = '';
+      cryptedLink.href = url.href;
+      cryptedLink.textContent = url.href;
+      cryptedLink.classList.remove('error');
+    } else {
+      cryptedLink.textContent = crypted.status;
+      cryptedLink.removeAttribute('href');
+      cryptedLink.classList.add('error');
     }
-    url.search += crypted.cipher;
-    url.hash = '';
-    cryptedLink.href = url.href;
-    cryptedLink.textContent = url.href;
-    cryptedLink.classList.remove('error');
-  } else {
-    cryptedLink.textContent = crypted.status;
-    cryptedLink.removeAttribute('href');
-    cryptedLink.classList.add('error');
-  }
-});
+  });
+};
 
-decPass.addEventListener('keydown', e => {
-  if (e.key === 'Enter') {
-    decBtn.click();
-  }
-});
+const setupDecPage = () => {
+  decPage.classList.remove('hidden');
 
-decBtn.addEventListener('click', e => {
-  const passPhrase = prefix + decPass.value;
-  const privateKey = cryptico.generateRSAKey(passPhrase, BITS);
-  const publicKey = cryptico.publicKeyString(privateKey);
-  const cipher = data;
-  const result = cryptico.decrypt(cipher, privateKey);
-  if (result.status === 'success') {
-    decryptedText.textContent = decodeURI(result.plaintext);
-    decryptedText.classList.add('decrypted');
-    decryptedText.classList.remove('error');
-  } else {
-    decryptedText.textContent = result.status;
-    decryptedText.classList.add('error');
-  }
-});
+  decryptedText.textContent = location.search.substr(1);
+
+  decPass.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+      decBtn.click();
+    }
+  });
+
+  decBtn.addEventListener('click', e => {
+    const passPhrase = prefix + decPass.value;
+    const privateKey = cryptico.generateRSAKey(passPhrase, BITS);
+    const publicKey = cryptico.publicKeyString(privateKey);
+    const cipher = data;
+    const result = cryptico.decrypt(cipher, privateKey);
+    if (result.status === 'success') {
+      decryptedText.textContent = decodeURI(result.plaintext);
+      decryptedText.classList.add('decrypted');
+      decryptedText.classList.remove('error');
+    } else {
+      decryptedText.textContent = result.status;
+      decryptedText.classList.add('error');
+    }
+  });
+};
+
+if (location.search.startsWith('?enc')) {
+  setupEncPage();
+} else if (location.search.startsWith('?dec')) {
+  setupDecPage();
+} else {
+  setupTopPage();
+}
 
